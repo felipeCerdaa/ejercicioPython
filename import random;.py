7b84064ed2cc3d2908e2ff42f23b2a9b310a8e3e
Felipe Cerda Arancibia <felipecerdaa12@gmail.com>
2024-05-27 21:06:38 -0400

    segundoCommit

--- import random;.py
+++ import random;.py
@@ -12,3 +12,36 @@ import random;
     Spock vaporiza a piedra, y como siempre, 
     piedra aplasta a tijera
 '''
+
+
+def select():
+    print("tijera, papel, piedra, lagarto, Spock")
+    player1 = input("ingrese su eleccion: ")
+    player1 = player1.lower()
+    return player1
+
+def player_compute():
+    player_compute = random.choice(["tijera", "papel", "piedra", "lagarto", "Spock"])
+    return player_compute
+
+def game(player1, player_compute):
+    if(player1 == player_compute):
+        print("empate")
+    elif(player1 == "tijera" and player_compute == "papel" or player1 == "tijera" and player_compute == "lagarto"):
+        print("gana player1 => ", player1 , " => ", player_compute)
+    elif(player1 == "papel" and player_compute == "piedra" or player1 == "papel" and player_compute == "Spock"):
+        print("gana player1 => ", player1 , " => ", player_compute)
+    elif(player1 == "piedra" and player_compute == "lagarto" or player1 == "piedra" and player_compute == "tijera"):
+        print("gana player1 => ", player1 , " => ", player_compute)
+    elif(player1 == "lagarto" and player_compute == "spock" or player1 == "lagarto" and player_compute == "papel"):
+        print("gana player1 => ", player1 , " => ", player_compute)
+    elif(player1 == "spock" and player_compute == "tijera" or player1 == "spock" and player_compute == "piedra"):
+        print("gana player1 => ", player1 , " => ", player_compute)
+    else:
+        print("gana player_compute => ", player_compute, " => ", player1)
+
+
+
+""" select()
+player_compute() """
+game(select(), player_compute())
