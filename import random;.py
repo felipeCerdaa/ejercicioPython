92e6569c6c0ee8a10f025f1b52a457f7923180d8
Felipe Cerda Arancibia <felipecerdaa12@gmail.com>
2024-05-27 20:57:37 -0400

    primer commit

---
+++ import random;.py
@@ -0,0 +1,14 @@
+import random;
+
+'''
+    Tijera corta a papel, 
+    papel tapa a piedra, 
+    piedra aplasta a lagarto, 
+    lagarto envenena a Spock, 
+    Spock rompe a tijera, 
+    tijera decapita a lagarto, 
+    lagarto devora a papel, 
+    papel desautoriza a Spock, 
+    Spock vaporiza a piedra, y como siempre, 
+    piedra aplasta a tijera
+'''
